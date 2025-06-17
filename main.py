067e3e856c8e34ee8eaf796094641b50445210ed
rayanagott <gottschall5566@uwlax.edu>
2025-06-17 17:15:15 -0500

    updates

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from navigation import open_home
 from navigation import open_pet
 
-open_pet()
 open_home()
+open_pet()
 
 tk.mainloop()
--- ui/pet_widget.py
+++ ui/pet_widget.py
@@ -5,10 +5,10 @@ def click_pet(event=None):
     print("test")
 
 def open_pet():
-    root = tk.Toplevel()
-    root.overrideredirect(True) # remove title bar
-    root.wm_attributes("-topmost", True) # pet is always on top
-    root.wm_attributes("-transparentcolor", "green") # make the background transparent (in green areas)
+    pet_window = tk.Toplevel()
+    pet_window.overrideredirect(True) # remove title bar
+    pet_window.wm_attributes("-topmost", True) # pet is always on top
+    pet_window.wm_attributes("-transparentcolor", "green") # make the background transparent (in green areas)
 
     image = Image.open("assets/stand_left.png").convert("RGBA")
     scale_factor = 2.5  # re-size by 2.5
@@ -20,29 +20,26 @@ def open_pet():
 
     standing_left = ImageTk.PhotoImage(image) #starting 'state'
 
-    # Canvas instead of label
-    canvas = tk.Canvas(root, width=new_width, height=new_height, bg="green", highlightthickness=0)
+    # use canvas
+    canvas = tk.Canvas(pet_window, width=new_width, height=new_height, bg="green", highlightthickness=0)
     canvas.pack()
-
-    # Draw image on canvas
     canvas.create_image(0, 0, anchor="nw", image=standing_left)
 
-    # Create transparent clickable rectangle over image
+    # transparent clickable rectangle over image
     canvas.tag_bind("click_area", "<Button-1>", click_pet)
     canvas.create_rectangle(0, 0, new_width, new_height, outline="", fill="", tags="click_area")
 
     # corner positioning - get screen info
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+    screen_width = pet_window.winfo_screenwidth()
+    screen_height = pet_window.winfo_screenheight()
     window_width = standing_left.width()
     window_height = standing_left.height()
     x = 20
     y = screen_height - window_height - 35 # pet sits above nav bar on my own computer
-    root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+    pet_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
     # idle pet logic
 
     # interactive pet logic
 
-    root.mainloop()
-   
+    tk.mainloop()
